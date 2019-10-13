384e6656f22dcdae5ecbfa0577fee032d01d39f7
saman828 <56400695+saman828@users.noreply.github.com>
2019-10-12 21:04:05 -0700

    Add files via upload

---
+++ Service1/js/jquery_003.js
@@ -0,0 +1,19 @@
+﻿(function ($) {
+	$.keypad.regionalOptions['fa'] = {
+		buttonText: '...', buttonStatus: '...!',
+		closeText: 'بستن', closeStatus: 'بستن',
+		clearText: 'حذف', clearStatus: 'حذف',
+		backText: 'ويرايش', backStatus: 'ويرايش',
+		shiftText: 'Shift', shiftStatus: 'Shift!',
+		spacebarText: '&nbsp;', spacebarStatus: '',
+		enterText: 'Entrar', enterStatus: '',
+		tabText: '→', tabStatus: '',
+		alphabeticLayout: $.keypad.qwertyAlphabetic,
+		fullLayout: $.keypad.qwertyLayout,
+		isAlphabetic: $.keypad.isAlphabetic,
+		isNumeric: $.keypad.isNumeric,
+		toUpper: $.keypad.toUpper,
+		isRTL: true,
+	};
+	$.keypad.setDefaults($.keypad.regionalOptions['fa']);
+})(jQuery);
